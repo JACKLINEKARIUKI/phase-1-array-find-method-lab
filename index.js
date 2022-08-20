fce18386d1ffc1013df3142ee48a8bfdf8d7ec3c
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-20 22:52:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record){
+    const yy = record.find(element => element.result === "W"); 
+    if (!yy) return undefined // early return
+    return yy.year
+}
+
+
